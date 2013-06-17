6ec4fb38373d31650945ab813252e2ced0a4b5a3
nyver <nyver@mail.ru>
2013-06-17 16:50:12 +0400

    Added diff command to files table popup menu

---
+++ src/main/java/com/nyver/rctool/menu/vcsPropertiesTablePopupMenu.java
@@ -0,0 +1,50 @@
+package com.nyver.rctool.menu;
+
+import com.nyver.rctool.RCTool;
+import com.nyver.rctool.listener.VcsPropertiesMouseListener;
+import com.nyver.rctool.worker.VcsDiffWorker;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ * @author Yuri Novitsky
+ */
+public class VcsPropertiesTablePopupMenu extends JPopupMenu
+{
+    public static String CMD_DIFF = "Diff with previous revision";
+    private RCTool mainFrame;
+
+    public VcsPropertiesTablePopupMenu(final RCTool mainFrame) {
+        super();
+
+        this.mainFrame = mainFrame;
+
+        JMenuItem menuItem = new JMenuItem(CMD_DIFF);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int row = mainFrame.getVcsPropertiesTable().getSelectedRow();
+                int rowRevision = mainFrame.getVcsTreeTable().getSelectedRow();
+                if ((row >= 0) && (rowRevision >= 0)) {
+                    String path = (String) mainFrame.getVcsPropertiesTable().getValueAt(row, 1);
+                    String revision = (String) mainFrame.getVcsTreeTable().getValueAt(rowRevision, 0);
+                    if (!path.isEmpty() && !revision.isEmpty()) {
+                        new VcsDiffWorker(path, revision, mainFrame).execute();
+                    }
+                }
+            }
+        });
+        add(menuItem);
+    }
+
+    public VcsPropertiesTablePopupMenu(String label) {
+        super(label);
+    }
+
+
+}
